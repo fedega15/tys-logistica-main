df6f459e895192955fdccd1d83e1da24133aed73
Federico Acosta <fede-2800@hotmail.com>
2023-03-17 11:04:30 -0300

    Acomodo de frm

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,26 +1,24 @@
 import { useForm } from "../../hooks/useForm"
-
+import { UserLogin } from "../../api/Model/Login"
 const initialForm = {
-    useremail:"",
-    password:"",
+    email: "",
+    password: "",
 }
 
-const vaildateForm =(form) => {
-
+const vaildateForm = (form) => {
     let errors = {}
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
-    let regexPassword = /^\?=.{8,}$/
-   
-    if (!form.useremail.trim()) {
-        errors.useremail = "el email es requerido"
-    }else if (!regexEmail.test(form.useremail.trim())) {
-        errors.useremail = "El campo email debe tener @ y un .com"
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    //let regexPassword = /^\?=.{3,}$/
+
+    if (!form.email.trim()) {
+        errors.email = "el email es requerido"
+    } else if (!regexEmail.test(form.email.trim())) {
+        errors.email = "El campo email debe tener @ y un .com"
     }
-    
     if (!form.password.trim()) {
         errors.password = "el password es requerido"
-    }else if (!regexPassword.test(form.password.trim())) {
-        errors.password = "El campo email debe tener mas de 8 caractes"
+    } else if (String(form.password).trim().length < 3) {
+        errors.password = "El campo password debe tener mas de 3 caractes"
     }
     return errors
 }
@@ -33,63 +31,65 @@ const ContactForm = () => {
         handleChange,
         handleBlur,
         handleSubmit,
-        } = useForm(initialForm, vaildateForm)
+    } = useForm(initialForm, vaildateForm)
 
     const handleFetch = async (e) => {
+        try {
             e.preventDefault()
-            try {
-                if (asd) {
-                    const api_response = await newUser(credenciales)
-                    if (api_response.status === 200) {
-                        const { data } = api_response
-                        console.log(data)
-                    }
-                } console.log(errors, correct)
-            } catch (error) {
-                console.log(error)
+            if (errors.hasOwnProperty('email')
+                ||errors.hasOwnProperty('password')) {
+                //hay errores
+                console.log(errors)
+                return false
             }
+            const api_response = await UserLogin(initialForm)
+            if (api_response.status === 200) {
+                const { data } = api_response
+                console.log(data)
+            }
+        } catch (error) {
+            console.log(error)
         }
-  
+    }
 
-return (
-<div className='container'>
+
+    return (
+        <div className='container'>
             <div className="container ">
                 <div className="col-md-11 mt-5">
-
                     <form onSubmit={handleSubmit} className='position-relative'>
                         <h4 className="mb-3">Iniciar sesion</h4>
                         <div className="row">
                             <div className="form-group col-md-6 mb-3">
-                                <label htmlFor="useremail">Email</label>
-                                <input  
-                                type='text' 
-                                name="useremail"
-                                placeholder="Tys@logistica.com.ar"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={form.useremail}                                   
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type='email'
+                                    name="email"
+                                    placeholder="Tys@logistica.com.ar"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={form.email}
                                 />
-                                {errors.useremail && <p>{errors.useremail}</p>}
+                                {errors.email && <p>{errors.email}</p>}
                             </div>
                         </div>
                         <div className="row">
                             <div className="form-group col-md-6 mb-3">
                                 <label htmlFor="password">Contraseña</label>
-                                <input  
-                                type='text' 
-                                name="password" 
-                                placeholder="*************"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={form.password}
+                                <input
+                                    type='password'
+                                    name="password"
+                                    placeholder="*************"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={form.password}
                                 />
-                                    {errors.password && <p>{errors.password}</p>}
+                                {errors.password && <p>{errors.password}</p>}
                             </div>
                         </div>
                         <br />
                         <div className="form-group">
                             <button onClick={handleFetch}>
-                                
                                 Enviar
                             </button>
                         </div>
@@ -97,6 +97,7 @@ return (
                 </div>
             </div>
         </div>
-        )}
+    )
+}
 
-        export default ContactForm
+export default ContactForm
