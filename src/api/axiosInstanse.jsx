c027d968066d7417706e4cb770ddefb9ca0b12ae
3iernes <fede-2800@hotmail.com>
2023-04-19 12:05:06 -0300

    Archivo de configuracion

--- src/api/axiosInstanse.jsx
+++ src/api/axiosInstanse.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+import { API_URL } from "../config";
 const customAxiosPrivate = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: API_URL,
   //headers:{ 'Content-Type' : 'application/json'},
   withCredentials: true,
 });
